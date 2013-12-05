161d6985840e4cb1cc9d40c5cc034ddb7044b758
hefnrh <516860685@qq.com>
2013-12-05 22:28:37 +0800

    bad test

--- src/run/MillionFunction.java
+++ src/run/MillionFunction.java
@@ -1,21 +1,42 @@
 package run;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 import abstractmath.Field28;
 
-public class MillionFunction {
+final public class MillionFunction implements Runnable {
 	private int xFunction = 0, yFunction = 0;
+	final Object lock;
+	final Random r;
+	final CountDownLatch cdl;
 	private int x = 0, y = 0;
-	private int[] highRankXFunction, highRankYFunction;
-	private double[] deviation;
-	final Random r = new Random();
+	final private int[] highRankXFunction, highRankYFunction;
+	final private double[] deviation;
 	final Field28 field = new Field28((byte) 0b1000_1101);
 	final byte[][] matrix = { { 2, 3, 1, 1 }, { 1, 2, 3, 1 }, { 1, 1, 2, 3 },
 			{ 3, 1, 1, 2 } };
+	final int runTime;
+	final int FunctionNumbers;
+	final Object rLock;
+
+	public MillionFunction(final int[] hrxf, final int[] hryf,
+			final double[] d, final Object lock, final CountDownLatch cdl,
+			final int runTime, final int FunctionNumbers, final Random r,
+			final Object rLock) {
+		highRankXFunction = hrxf;
+		highRankYFunction = hryf;
+		deviation = d;
+		this.lock = lock;
+		this.cdl = cdl;
+		this.runTime = runTime;
+		this.FunctionNumbers = FunctionNumbers;
+		this.r = r;
+		this.rLock = rLock;
+	}
 
 	final private byte[] createA() { // create a0~a3
-
 		x = r.nextInt();
 		int integer = x;
 		byte a[] = new byte[4];
@@ -23,20 +44,62 @@ public class MillionFunction {
 			a[i] = (byte) integer;
 			integer >>= 8;
 		}
+
 		return a;
 	}
 
-	final private void createY(byte[] a) {
-		byte[] out = field.AESMixColumns(matrix, a);
+	final private void createY(final byte[] a) {
+		final byte[] out = field.AESMixColumns(matrix, a);
 		y = out[0] & 0b1111_1111;
 		y |= (out[1] & 0b1111_1111) << 8;
 		y |= (out[2] & 0b1111_1111) << 16;
 		y |= (out[3] & 0b1111_1111) << 24;
 	}
 
-	final private void createFunctions() {
-		xFunction = r.nextInt();
-		yFunction = r.nextInt();
+	final private void createFunctions(final int k) {
+		if (/*k > 31*/ true) {
+			xFunction = r.nextInt();
+			yFunction = r.nextInt();
+		} else {
+			yFunction = 1;
+			yFunction <<= k;
+			switch (k / 8) {
+			case 0:
+				if (k % 8 == 0) {
+					xFunction = 0b00000001000000010000000100000000;
+				} else {
+					xFunction = 0b00000010000000100000001100000001;
+					xFunction <<= ((k % 8) - 1);
+				}
+				break;
+			case 1:
+				if (k % 8 == 0) {
+					xFunction = 0b00000001000000010000000000000001;
+				} else {
+					xFunction = 0b00000010000000110000000100000001;
+					xFunction <<= ((k % 8) - 1);
+				}
+				break;
+			case 2:
+				if (k % 8 == 0) {
+					xFunction = 0b00000001000000000000000100000001;
+				} else {
+					xFunction = 0b00000011000000100000000100000001;
+					xFunction <<= ((k % 8) - 1);
+				}
+				break;
+			case 3:
+				if (k % 8 == 0) {
+					xFunction = 0b00000000000000010000000100000001;
+				} else {
+					xFunction = 0b00000001000000100000001000000011;
+					xFunction <<= ((k % 8) - 1);
+				}
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
 	final public void displayx() {
@@ -47,50 +110,68 @@ public class MillionFunction {
 		System.out.println("y = " + y);
 	}
 
-	final public double calcDeviation(int runTime) {
+	final public double calcDeviation(final int runTime) {
 		double simpleTime = 0;
 
 		for (int i = 0; i < runTime; ++i) {
-                        createY(createA());
-			int answer0 = x & xFunction;
-			int answer1 = y & yFunction;
-			byte sign0 = 0,sign1 = 0;
+
+			int answer0 = x ^ xFunction;
+			int answer1 = y ^ yFunction;
+			byte sign0 = 0, sign1 = 0;
 			for (int j = 0; j < 32; ++j) {
-				if((answer0 & 1) ==1) ++sign0;
-    			        if((answer1 & 1) ==1) ++sign1;
+				if ((answer0 & 1) == 1)
+					sign0 ^= 1;
+				if ((answer1 & 1) == 1)
+					sign1 ^= 1;
 				answer0 >>= 1;
 				answer1 >>= 1;
 			}
-			if (((sign0 + sign1) & 1) == 1)
+			if (((sign1 ^ sign0) & 1) == 1)
 				++simpleTime;
 		}
-		return Math.abs(simpleTime / runTime - 0.5);
+		return Math.abs(((double)simpleTime) / ((double) runTime) - 0.5);
 	}
 
-	final void sort(double deviationx) {
-
-		int i = 0;
-		while (i < 10) {
-			if (deviationx > deviation[i]) {
-				for (int j = 9; j > i; --j) {
-					highRankXFunction[j] = highRankXFunction[j - 1];
-					highRankYFunction[j] = highRankYFunction[j - 1];
-					deviation[j] = deviation[j - 1];
-				}
-				highRankXFunction[i] = xFunction;
-				highRankYFunction[i] = yFunction;
-				deviation[i] = (float) deviationx;
+	final public void sort(final double deviationx) {
+		int indexToInsert;
+		int i;
+		synchronized (lock) {
+			indexToInsert = Arrays.binarySearch(deviation, deviationx);
+			if (indexToInsert == -1 || indexToInsert == 0)
+				return;
+			if (indexToInsert < 0)
+				indexToInsert = -indexToInsert - 2;
+			else {
+				while (indexToInsert > 0
+						&& deviation[indexToInsert - 1] == deviationx)
+					--indexToInsert;
+				--indexToInsert;
+				if (indexToInsert < 0)
+					return;
 			}
-			++i;
+			for (i = 0; i < indexToInsert; ++i) {
+				deviation[i] = deviation[i + 1];
+				highRankXFunction[i] = highRankXFunction[i + 1];
+				highRankYFunction[i] = highRankYFunction[i + 1];
+			}
+			deviation[indexToInsert] = deviationx;
+			highRankXFunction[indexToInsert] = xFunction;
+			highRankYFunction[indexToInsert] = yFunction;
 		}
-
 	}
 
-	final public void runFuctions(int runTime, int FunctionNmubers) {
-		for (int k = 0; k < FunctionNmubers; ++k) {
-			createFunctions();
+	final public void runFuctions() {
+		for (int k = 0; k < FunctionNumbers; ++k) {
+			createY(createA());
+			createFunctions(k);
 			sort(calcDeviation(runTime));
 		}
 
+	}
+
+	@Override
+	final public void run() {
+		runFuctions();
+		cdl.countDown();
 	}
 }
--- src/tools/Test.java
+++ src/tools/Test.java
@@ -1,16 +1,21 @@
 package tools;
 
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
+import run.MillionFunction;
 import abstractmath.Field28;
 
-public class Test {
-	public static void main(String[] args) {
-//		multiThread(1);
-//		int n = Integer.parseInt(args[0]);
-//		multiThread(n);
-		mixTest(1 << 24);
+final public class Test {
+	public static final void main(String[] args) {
+		// multiThread(1);
+		// int n = Integer.parseInt(args[0]);
+		// multiThread(n);
+		// mixTest(1 << 24);
+		runMillionFunction(1, 10000, 100);
+//		testSort();
 	}
+
 	public static final void multiThread(int n) {
 		System.out.println("-----------test multiple thread------------");
 		byte p = 0b0001110;
@@ -31,6 +36,7 @@ public class Test {
 			e.printStackTrace();
 		}
 	}
+
 	public static final void singleThread() {
 		System.out.println("-----------test single thread------------");
 		byte b = 0b001110;
@@ -47,8 +53,10 @@ public class Test {
 		}
 		long end = System.currentTimeMillis();
 		System.out.println("after multiply b = " + toBin(b));
-		System.out.println("time used for 1000000 mutiply: " + (end - start) + " ms");
+		System.out.println("time used for 1000000 mutiply: " + (end - start)
+				+ " ms");
 	}
+
 	public static final String toBin(byte b) {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < 8; ++i) {
@@ -56,14 +64,10 @@ public class Test {
 		}
 		return sb.toString();
 	}
-	
+
 	public static final void mixTest(final int n) {
-		final byte[][] matrix = {
-				{ 2, 3, 1, 1 },
-				{ 1, 2, 3, 1 },
-				{ 1, 1, 2, 3 },
-				{ 3, 1, 1, 2 }
-		};
+		final byte[][] matrix = { { 2, 3, 1, 1 }, { 1, 2, 3, 1 },
+				{ 1, 1, 2, 3 }, { 3, 1, 1, 2 } };
 		final byte p = (byte) 0b10001101;
 		final Field28 f = new Field28(p);
 		byte[] data = new byte[4];
@@ -73,6 +77,78 @@ public class Test {
 			data = f.AESMixColumns(matrix, data);
 		}
 		long end = System.currentTimeMillis();
-		System.out.println(n + " times " + "time used: " + (end - start) + " ms");
+		System.out.println(n + " times " + "time used: " + (end - start)
+				+ " ms");
 	}
+
+	public static final void runMillionFunction(final int nThread,
+			final int runTime, final int functionNumber) {
+		final int length = 20;
+		final Object lock = new Object(), rLock = new Object();
+		final CountDownLatch cdl = new CountDownLatch(nThread);
+		final int[] highRankXFunction = new int[length];
+		final int[] highRankYFunction = new int[length];
+		final double[] deviation = new double[length];
+		final MillionFunction[] mf = new MillionFunction[nThread];
+		final Random r = new Random();
+		for (int i = 0; i < length; ++i) {
+			highRankXFunction[i] = 0;
+			highRankYFunction[i] = 0;
+			deviation[i] = -1;
+		}
+		for (int i = 0; i < nThread; ++i) {
+			mf[i] = new MillionFunction(highRankXFunction, highRankYFunction,
+					deviation, lock, cdl, runTime, functionNumber, r, rLock);
+		}
+		final Thread[] thread = new Thread[nThread];
+		for (int i = 0; i < nThread; ++i)
+			thread[i] = new Thread(mf[i]);
+		int i = 0;
+		final long start = System.currentTimeMillis();
+		for (; i < nThread; ++i)
+			thread[i].start();
+		try {
+			cdl.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		final long end = System.currentTimeMillis();
+		String msg = nThread + " threads * " + runTime + " input * "
+				+ functionNumber + " functions: " + (end - start) + " ms";
+		System.out.println(msg);
+		for (i = 0; i < length; ++i) {
+			System.out.println(toBin(highRankXFunction[i]) + " = "
+					+ toBin(highRankYFunction[i]) + " : " + deviation[i]);
+		}
+	}
+
+	final static String toBin(int n) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 32; ++i) {
+			sb.append((n >> (31 - i)) & 1);
+		}
+		return sb.toString();
+	}
+	
+	final static void testSort() {
+		int[] hx = new int[10];
+		int[] hy = new int[10];
+		double[] d = new double[10];
+		for (int i = 0; i < 10; ++i) {
+			hx[i] = hy[i] = i;
+			d[i] = (int) (0.34 * (i + 100));
+		}
+		double tmp;
+		for (int i = 0; i < 10; ++i) {
+			System.out.print(d[i] + " ");
+		}
+		tmp = 36;
+		MillionFunction mf = new MillionFunction(hx, hy, d, new Object(), null, 0, 0, null, null);
+		System.out.println(tmp);
+		mf.sort(tmp);
+		for (int i = 0; i < 10; ++i) {
+			System.out.print(d[i] + " ");
+		}
+	}
+	
 }
